54e82254dbc62bef960751d0778d0cce9fc7331c
shivajaini <shiva.jaini@gmail.com>
2014-06-22 10:55:37 -0700

    Initial Commit

---
+++ fbstory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fbstory
+//
+//  Created by Shiva Jaini on 6/22/14.
+//  Copyright (c) 2014 Shiva Jaini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fbstory/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fbstory
+//
+//  Created by Shiva Jaini on 6/22/14.
+//  Copyright (c) 2014 Shiva Jaini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
